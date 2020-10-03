e6748a50c48ecacefd96c62440c780b7502eae24
Ironman32 <72316825+Ironman32@users.noreply.github.com>
2020-10-04 00:12:54 +0530

    Add files via upload

---
+++ FClient.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class FClient {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		Fraction f = new Fraction();
+		f.Start();
+
+	}
+
+}
